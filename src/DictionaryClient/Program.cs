aa35ce30490db8279c3cf0fd4e5ea61412f9d4e2
gorin-nv <gorin.nv@gmail.com>
2014-05-04 23:19:18 +0600

    [+] add client-server examples

---
+++ src/DictionaryClient/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace DictionaryClient
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var requester = 0;
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "exit")
+                    break;
+                if (input == "add")
+                {
+                    var id = requester++;
+                    new Thread(() =>
+                                   {
+                                       var gateway = new ServerGateway("127.0.0.1", 9050);
+                                       for (var i = 0; i < 100; i++)
+                                       {
+                                           var responce = gateway.Request(input);
+                                           if (responce.HasError)
+                                           {
+                                               var actual = Console.ForegroundColor;
+                                               Console.ForegroundColor = ConsoleColor.Red;
+                                               Console.WriteLine("id=" + id + ": server error: " + responce.Error);
+                                               Console.WriteLine(responce.Error);
+                                               Console.ForegroundColor = actual;
+                                           }
+                                           else
+                                           {
+                                               Console.WriteLine("id=" + id + ": " + responce.Text);
+                                           }
+                                           Thread.Sleep(1000);
+                                       }
+                                   }).Start();
+                }
+            }
+        }
+    }
+
+    internal class Responce
+    {
+        public static Responce Create(string text)
+        {
+            return new Responce {Text = text};
+        }
+
+        public static Responce CreateError(string message)
+        {
+            return new Responce {Error = message};
+        }
+
+        public string Text { get; private set; }
+        public string Error { get; private set; }
+
+        public bool HasError
+        {
+            get { return !string.IsNullOrEmpty(Error); }
+        }
+    }
+
+    internal class ServerGateway
+    {
+        private const int BufferLength = 1024;
+        private readonly byte[] _buffer;
+        private readonly IPEndPoint _ipep;
+
+        public ServerGateway(string ip, int port)
+        {
+            _ipep = new IPEndPoint(IPAddress.Parse(ip), port);
+            _buffer = new byte[BufferLength];
+        }
+
+        public Responce Request(string request)
+        {
+            var requestBuffer = Encoding.ASCII.GetBytes(request);
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    server.Connect(_ipep);
+                }
+                catch (SocketException e)
+                {
+                    return Responce.CreateError(e.ToString());
+                }
+                server.Send(requestBuffer);
+                var responce = server.Receive(_buffer);
+                var data = Encoding.ASCII.GetString(_buffer, 0, responce);
+                server.Shutdown(SocketShutdown.Both);
+                server.Close();
+                return Responce.Create(data);
+            }
+        }
+    }
+}
---
+++ src/DictionaryServer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace DictionaryServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var listener = new TcpListener(IPAddress.Any, 9050);
+            try
+            {
+                var active = true;
+                var inProcess = false;
+                listener.Start();
+                var thread = new Thread(() =>
+                {
+                    while (active)
+                    {
+                        var client = listener.AcceptTcpClient();
+                        inProcess = true;
+                        ThreadPool.QueueUserWorkItem(ThreadProc, client);
+                        inProcess = false;
+                    }
+                });
+                thread.Start();
+                Console.ReadLine();
+                active = false;
+                while (inProcess)
+                {
+                }
+                thread.Abort();
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+
+        private static void ThreadProc(object state)
+        {
+            var client = (TcpClient) state;
+            var inputBuf = new byte[1024];
+            var inputLenght = client.GetStream().Read(inputBuf, 0, inputBuf.Length);
+            var input = Encoding.ASCII.GetString(inputBuf, 0, inputLenght);
+
+            var text = "hello, " + input;
+            Thread.Sleep(3000);
+            var buffer = Encoding.ASCII.GetBytes(text);
+            client.GetStream().Write(buffer, 0, buffer.Length);
+            client.Close();
+
+            Console.WriteLine("Client connected");
+        }
+    }
+}
